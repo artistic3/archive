66f2fba115b15dd38c38dd1215c7f02bffbe1bcc
artistic3 <artisticsoftware3@gmail.com>
2025-01-14 09:13:38 +0000

    ...

--- 0115HV/agregate.php
+++ 0115HV/agregate.php
@@ -68,10 +68,10 @@ return [
 		/**
 		Race 8
 		*/
-		'favorites' => '(F) 2, 4, 12',
+		'favorites' => '(F) 2, 4, 11, 12',
 		'unions($10)' => '2, 4, 8, 12',
-		'union + favorites' => '2, 4, 8, 12',
-		'early 5' => '2, 4, 8, 12',
+		'union + favorites' => '2, 4, 8, 11, 12',
+		'early 5' => '2, 4, 8, 11, 12',
 	],
 	'9' => [
 		/**
