ddc9a3275a2a28a1adfc180824962ed62bd22187
artistic3 <artisticsoftware3@gmail.com>
2025-02-06 23:54:45 +0000

    ...

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
-for i in `seq 1 2641`; 
+for i in `seq 1 1369`; 
 do
-    wget https://raw.githubusercontent.com/artistic7/archive/refs/heads/main/history$i.php;
+    wget https://raw.githubusercontent.com/artistic8/archive/refs/heads/main/history$i.php;
 done
