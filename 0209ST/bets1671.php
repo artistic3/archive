8c11867c78e3926a03b7c379415298fd4a8ae498
artistic8 <aadel.iimam@gmail.com>
2025-02-08 12:35:08 +0800

    ...

---
+++ 0209ST/bets1671.php
@@ -0,0 +1,131 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '13',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
+		'win inter' => '3, 5, 11',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '2, 9',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
+		'win inter' => '1, 2, 4, 7, 9, 10',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '2, 10',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
+		'win inter' => '1, 5, 7, 12',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '2, 11',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
+		'win inter' => '1, 2, 3, 6, 11, 13',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '3, 11',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
+		'win inter' => '2, 3, 8',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '8',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
+		'win inter' => '1, 2, 4, 5, 6, 7, 8, 9, 10',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '7, 12',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
+		'win inter' => '',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '2, 7',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8',
+		'win inter' => '4, 5, 7',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '3',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
+		'win inter' => '1, 2, 3, 5, 6, 7, 9, 10, 11, 13',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'favorites' => '2, 4',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
+		'win inter' => '2, 3, 10',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+	'11' => [
+		/**
+		Race 11
+		*/
+		'favorites' => '2',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
+		'win inter' => '9, 10',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+];
+//total place end favorites: 0
+//total place end wp: 0
+//total place wp: 0
+//total sure place: 0
+//total win: 0
+//total: 0
