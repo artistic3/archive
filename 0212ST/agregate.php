6f553d3a8f2b044a96ab411d7790acb4b5b91361
artistic3 <artisticsoftware3@gmail.com>
2025-02-11 05:03:27 +0000

    ...

---
+++ 0212ST/agregate.php
@@ -0,0 +1,62 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '(F) 11',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '(F) 4, 10',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '(F) 4, 9',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '(F) 7, 9',
+		'qin(union 1)' => '2, 4, 7, 9',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '(F) 6',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '(F) 1',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '(F) 4, 9',
+		'qin(union 1)' => '3, 4, 7, 9',
+		'qin(union 2467)' => '3, 4, 6, 9',
+		'qin(union 3237)' => '2, 3, 4, 9',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '(F) 2',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '(F) 1, 5',
+	],
+];
