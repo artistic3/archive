cf5640ab536766dad2a062660a4ba9633d341ff0
artistic3 <artisticsoftware3@gmail.com>
2025-02-11 12:34:18 +0000

    ...

--- 0212ST/agregate.php
+++ 0212ST/agregate.php
@@ -5,7 +5,10 @@ return [
 		/**
 		Race 1
 		*/
-		'favorites' => '(F) 11',
+		'favorites' => '(F) 11, 12',
+		'qin(union 1754)' => '7, 8, 11, 12',
+		'unions($10)' => '7, 8, 11, 12',
+		'union + favorites' => '7, 8, 11, 12',
 	],
 	'2' => [
 		/**
@@ -31,27 +34,30 @@ return [
 		/**
 		Race 5
 		*/
-		'favorites' => '(F) 6',
+		'favorites' => '(F) 6, 7',
+		'qin(union 1)' => '1, 4, 6, 7',
+		'unions($10)' => '1, 4, 6, 7',
+		'union + favorites' => '1, 4, 6, 7',
 	],
 	'6' => [
 		/**
 		Race 6
 		*/
-		'favorites' => '(F) 1',
+		'favorites' => '(F) 1, 8',
 	],
 	'7' => [
 		/**
 		Race 7
 		*/
-		'favorites' => '(F) 4, 7, 8, 9',
+		'favorites' => '(F) 4, 5, 7, 8, 9',
 		'unions($10)' => '2, 3, 4, 6, 7, 8, 9',
-		'union + favorites' => '2, 3, 4, 6, 7, 8, 9',
+		'union + favorites' => '2, 3, 4, 5, 6, 7, 8, 9',
 	],
 	'8' => [
 		/**
 		Race 8
 		*/
-		'favorites' => '(F) 2, 9',
+		'favorites' => '(F) 2, 9, 10',
 	],
 	'9' => [
 		/**
--- 0212ST/bets1649.php
+++ 0212ST/bets1649.php
@@ -5,11 +5,11 @@ return [
 		/**
 		Race 1
 		*/
-		'favorites' => '11',
+		'favorites' => '11, 12',
 		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
-		'win inter' => '3, 7, 8, 9, 11',
-		'qin inter' => '2, 3, 5, 7, 8, 9, 10, 11',
-		'trio inter' => '1, 2, 3, 4, 5, 7, 8, 9, 10, 11',
+		'win inter' => '7',
+		'qin inter' => '3, 7',
+		'trio inter' => '3, 4, 7',
 		'bets' => [
 		],
 		'total bets' => 0,
@@ -57,22 +57,23 @@ return [
 		/**
 		Race 5
 		*/
-		'favorites' => '6',
+		'favorites' => '6, 7',
 		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
-		'win inter' => '1, 4, 10, 12',
+		'win inter' => '1, 4',
 		'qin inter' => '1, 4, 6, 10, 11, 12',
 		'trio inter' => '1, 2, 4, 6, 10, 11, 12',
 		'bets' => [
+			'qin(union 1649)' => '1, 4, 6, 7',
 		],
-		'total bets' => 0,
+		'total bets' => 60,
 	],
 	'6' => [
 		/**
 		Race 6
 		*/
-		'favorites' => '1',
+		'favorites' => '1, 8',
 		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
-		'win inter' => '1, 2, 4, 6, 8, 10, 11',
+		'win inter' => '1, 2, 4, 6, 8, 10',
 		'qin inter' => '1, 2, 3, 4, 6, 8, 10, 11, 12, 13',
 		'trio inter' => '1, 2, 3, 4, 6, 7, 8, 10, 11, 12, 13',
 		'bets' => [
@@ -83,11 +84,11 @@ return [
 		/**
 		Race 7
 		*/
-		'favorites' => '4, 7, 8, 9',
+		'favorites' => '4, 5, 7, 8, 9',
 		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9',
 		'win inter' => '3, 7',
 		'qin inter' => '3, 5, 6, 7',
-		'trio inter' => '1, 2, 3, 5, 6, 7, 9',
+		'trio inter' => '2, 3, 5, 6, 7, 9',
 		'bets' => [
 		],
 		'total bets' => 0,
@@ -96,10 +97,10 @@ return [
 		/**
 		Race 8
 		*/
-		'favorites' => '2, 9',
+		'favorites' => '2, 9, 10',
 		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
-		'win inter' => '1, 2, 4, 5, 9, 12',
-		'qin inter' => '1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12',
+		'win inter' => '1, 2, 5',
+		'qin inter' => '1, 2, 4, 5, 7, 8, 9, 10',
 		'trio inter' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
 		'bets' => [
 		],
@@ -119,5 +120,5 @@ return [
 		'total bets' => 0,
 	],
 ];
-//total qin: 0
+//total qin: -60
 //total: 0
