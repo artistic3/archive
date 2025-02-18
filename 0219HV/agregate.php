ab20ba1649558ded3f6cc9b839d27e1fc6664db2
artistic3 <artisticsoftware3@gmail.com>
2025-02-18 04:17:16 +0000

    ...

---
+++ 0219HV/agregate.php
@@ -0,0 +1,100 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '(F) 6',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '(F) 2, 7',
+		'qin(union )' => '2, 4, 7',
+		'unions($10)' => '2, 4, 7',
+		'union + favorites' => '2, 4, 7',
+		'count union + favorites' => '3',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '(F) 2, 3',
+		'qin(union 139)' => '2, 3, 5, 11',
+		'unions($10)' => '2, 3, 5, 7, 11, 12',
+		'union + favorites' => '2, 3, 5, 7, 11, 12',
+		'count union + favorites' => '6',
+		'qin(union 145)' => '2, 3, 5, 7',
+		'qin(union 157)' => '2, 3, 7, 11',
+		'qin(union 369)' => '2, 3, 5, 12',
+		'qin(union 381)' => '2, 3, 11, 12',
+		'qin(union 573)' => '2, 3, 7, 12',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '(F) 7',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '(F) 6, 12',
+		'qin(union )' => '4, 6, 12',
+		'unions($10)' => '4, 6, 12',
+		'union + favorites' => '4, 6, 12',
+		'count union + favorites' => '3',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '(F) 5, 10',
+		'qin(union 5)' => '2, 3, 5, 10',
+		'unions($10)' => '1, 2, 3, 5, 10',
+		'union + favorites' => '1, 2, 3, 5, 10',
+		'count union + favorites' => '5',
+		'qin(union 17)' => '1, 2, 5, 10',
+		'qin(union 32)' => '1, 3, 5, 10',
+		'qin(union 177)' => '2, 5, 10',
+		'qin(union 192)' => '3, 5, 10',
+		'qin(union 576)' => '1, 5, 10',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '(F) 5, 9',
+		'qin(union )' => '2, 5, 9',
+		'unions($10)' => '2, 5, 9',
+		'union + favorites' => '2, 5, 9',
+		'count union + favorites' => '3',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '(F) 4, 9',
+		'qin(union )' => '2, 4, 5, 9',
+		'unions($10)' => '2, 4, 5, 9',
+		'union + favorites' => '2, 4, 5, 9',
+		'count union + favorites' => '4',
+		'qin(union 6)' => '4, 5, 9',
+		'qin(union 23)' => '2, 4, 9',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '(F) 1, 12',
+		'qin(union )' => '1, 4, 9, 12',
+		'unions($10)' => '1, 4, 9, 12',
+		'union + favorites' => '1, 4, 9, 12',
+		'count union + favorites' => '4',
+		'qin(union 11)' => '1, 4, 12',
+		'qin(union 31)' => '1, 9, 12',
+	],
+];
