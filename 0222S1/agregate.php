60aec7312fd0129a7251bef4f56194885b2cc916
artistic8 <aadel.iimam@gmail.com>
2025-02-21 16:14:57 +0800

    ...

--- 0222S1/agregate.php
+++ 0222S1/agregate.php
@@ -28,7 +28,7 @@ return [
 		/**
 		Race 4
 		*/
-		'favorites' => '(F) 1, 3, 5, 10, 15',
+		'favorites' => '(F) 1, 2, 3, 5, 10, 15',
 		'unions($10)' => '1, 2, 5, 6, 10, 11',
 		'union + favorites' => '1, 2, 3, 5, 6, 10, 11, 15',
 		'count union + favorites' => '8',
