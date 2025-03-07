768a7525b1e69082a6560930bc2ac99803a174ef
artistic8 <aadel.iimam@gmail.com>
2025-03-07 12:29:56 +0800

    ...

---
+++ 0308S2/agregate.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '(F) 1, 4',
+		'qin(union )' => '1, 3, 4, 5',
+		'unions($10)' => '1, 3, 4, 5',
+		'union + favorites' => '1, 3, 4, 5',
+		'count union + favorites' => '4',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '(F) 1',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '(F) 5, 6',
+		'qin(union 1008)' => '4, 5, 6, 8',
+		'unions($10)' => '1, 4, 5, 6, 8',
+		'union + favorites' => '1, 4, 5, 6, 8',
+		'count union + favorites' => '5',
+		'qin(union 101)' => '1, 5, 6, 8',
+		'qin(union 1020)' => '1, 4, 5, 6',
+	],
+];
---
+++ 0308S2/bets873.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '1, 4',
+		'runners' => '1, 2, 3, 4, 5, 6, 7',
+		'win inter' => '1, 3, 4, 5',
+		'qin inter' => '1, 2, 3, 4, 5, 6, 7',
+		'trio inter' => '1, 2, 3, 4, 5, 6, 7',
+		'bets' => [
+			'qin(union 873)' => '1, 3, 4, 5',
+		],
+		'total bets' => 60,
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '1',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
+		'win inter' => '1, 2, 3, 6, 7, 10',
+		'qin inter' => '1, 2, 3, 4, 5, 6, 7, 8, 10',
+		'trio inter' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '5, 6',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15',
+		'win inter' => '1, 4',
+		'qin inter' => '1, 3, 4, 5, 6, 8',
+		'trio inter' => '1, 2, 3, 4, 5, 6, 8',
+		'bets' => [
+			'qin(union 873)' => '1, 4, 5, 6',
+		],
+		'total bets' => 60,
+	],
+];
+//total qin: -120
+//total: 0
