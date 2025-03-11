1f3cb8bea5e25b1273f2f529b8248041f3c7d4fa
artistic software <aadel.iimam@gmail.com>
2025-03-11 04:16:20 +0000

    ...

---
+++ 0312HV/agregate.php
@@ -0,0 +1,65 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '(F) 9',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '(F) 5, 11',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '(F) 2, 7',
+		'qin(union )' => '2, 5, 7, 11',
+		'unions($10)' => '2, 5, 7, 11',
+		'union + favorites' => '2, 5, 7, 11',
+		'count union + favorites' => '4',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '(F) 2, 5',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '(F) 1',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '(F) 8, 10',
+		'qin(seven )' => '1, 2, 3, 4, 7, 8, 10',
+		'sevens($10)' => '1, 2, 3, 4, 7, 8, 10',
+		'count sevens' => '7',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '(F) 6',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '(F) 5',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '(F) 8, 10',
+	],
+];
