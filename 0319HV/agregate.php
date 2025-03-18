b41de6113471ad07c69f5b98f54c8662f26d39fb
artistic8 <aadel.iimam@gmail.com>
2025-03-18 13:08:16 +0800

    ...

--- 0319HV/agregate.php
+++ 0319HV/agregate.php
@@ -5,7 +5,7 @@ return [
 		/**
 		Race 1
 		*/
-		'favorites' => '(F) 4, 10',
+		'favorites' => '(F) 3, 4, 10',
 		'qin(union )' => '3, 4, 8, 10',
 		'unions($10)' => '3, 4, 8, 10',
 		'union + favorites' => '3, 4, 8, 10',
--- 0319HV/bets620.php
+++ 0319HV/bets620.php
@@ -5,10 +5,10 @@ return [
 		/**
 		Race 1
 		*/
-		'favorites' => '4, 10',
+		'favorites' => '3, 4, 10',
 		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
-		'win inter' => '3, 4, 8, 10',
-		'qin inter' => '3, 4, 6, 8, 10',
+		'win inter' => '3, 4, 8',
+		'qin inter' => '3, 4, 6, 8',
 		'trio inter' => '3, 4, 5, 6, 8, 9, 10, 12',
 		'bets' => [
 			'qin(union 620)' => '3, 4, 8, 10',
